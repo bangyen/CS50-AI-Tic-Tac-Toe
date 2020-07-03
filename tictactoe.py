56169bd16c4181e58d143e4bfa25df3441175d85
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2020-07-03 02:27:54 -0400

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -13,27 +13,16 @@ def initial_state():
     """
     Returns starting state of the board.
     """
-    return [[EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY]]
+    return [[EMPTY] * 3 for _ in range(3)]
 
 
 def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    count = 0
-    for i in range(3):
-        for j in range(3):
-            if board[i][j] != EMPTY:
-                count += 1
+    count = sum(sum(map(bool, row)) for row in board)
     
-    if board == initial_state():
-        return X
-    if count % 2 == 1:
-        return O
-    else:
-        return X
+    return [X, O][count % 2]
     
 
 
@@ -41,12 +30,10 @@ def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    moves = set()
-    for i in range(3):
-        for j in range(3):
-            if board[i][j] == EMPTY:
-                moves.add((i,j))
-    return moves
+    return {
+        (i, j) for i in range(3)
+        for j in range(3) if board[i][j]
+    }
 
 
 def result(board, action):
@@ -67,45 +54,27 @@ def winner(board):
     Returns the winner of the game, if there is one.
     """
     for i in range(3):
-        if board[i][0] == board[i][1] == board[i][2]:
-            if board[i][0] == X:
-                return X
-            elif board[i][0] == O:
-                return O
-            else:
-                return None
+        if board[i] == [X] * 3:
+            return X
+        elif board[i] == [O] * 3:
+            return O
     for j in range(3):
-        if board[0][j] == board[1][j] == board[2][j]:
-            if board[0][j] == X:
-                return X
-            elif board[0][j] == O:
-                return O
-            else:
-                return None
+        col = [board[i][j] for i in range(3)]
+        if col == [X] * 3:
+            return X
+        elif col == [O] * 3:
+            return O
     if board[0][0] == board[1][1] == board[2][2]:
-        if board[0][0] == X:
-            return X
-        elif board[0][0] == O:
-            return O
-        else:
-            return None
+        return board[0][0]
     if board[2][0] == board[1][1] == board[0][2]:
-        if board[2][0] == X:
-            return X
-        elif board[2][0] == O:
-            return O
-        else:
-            return None
-    return None
+        return board[2][0]
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    if (winner(board) == X):
-        return True
-    elif (winner(board) == O):
+    if winner(board) in [X, O]:
         return True
         
     for i in range(3):
@@ -120,9 +89,9 @@ def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    if (winner(board) == X):
+    if winner(board) == X:
         return 1
-    elif (winner(board) == O):
+    elif winner(board) == O:
         return -1
     else:
         return 0
